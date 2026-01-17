aa52553c184a333c07d80827e4736232c5381242
Angus Jull <angusjull1@gmail.com>
2026-01-17 09:42:30 -0500

    Add default map

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Map from './Map.tsx'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -18,6 +19,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
+        <Map />
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
