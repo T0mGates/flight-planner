5134c00251f18ff8b8834c566af75b3246c00956
Vitaly Dufour <vitdufour@gmail.com>
2026-01-17 11:22:06 -0500

    Reformatting

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import './App.css'
 import { useQuery }     from '@tanstack/react-query';
-import Map              from './Map.tsx'
-import FlightInfoCard   from './FlightInfoCard.tsx'
+import Map              from './components/flight/Map.tsx'
+import FlightInfoCard   from './components/flight/FlightInfoCard.tsx'
 
 const fetchFlights = async () => {
   const response = await fetch('http://127.0.0.1:8000/flights');
@@ -20,17 +20,15 @@ function App() {
   if (isLoading) return <div>Loading flights...</div>;
   if (error) return <div>Error loading flight data!</div>;
 
-  console.log(data)
-
   return (
-    <>
+    <div className="w-full h-full">
       <div className="card flex flex-row gap-2 justify-center">
         <Map />
         <div className="w-100">
           <FlightInfoCard flights={Object.values(data)}/>
         </div>
       </div>
-    </>
+    </div>
   )
 }
 
